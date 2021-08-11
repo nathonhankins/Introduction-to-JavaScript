43b89c9235a00f21fcd82370730271f725650c7f
nathon pressley <nathon990@gmail.com>
2021-08-10 20:18:38 -0800

    final error

--- index.js
+++ index.js
@@ -217,10 +217,19 @@ Using the grade function below do the following:
  below should return 'you got an F'
 */
 
-function grade(100) {
-  if ()
+function grade(score) {
+  if (score <= 100 && score >= 90) {
+    return "you got an A";
+  } else if (score <= 89 && score >= 80) {
+    return "you got a B";
+  } else if (score <= 79 && score >= 70) {
+    return "you got a C";
+  } else if (score <= 69 && score >= 60) {
+    return "you got a D";
+  } else if (score <= 59) {
+    return "you got an F";
+  }
 }
-
 /*💪💪💪💪💪💪💪💪💪💪 Stretch 💪💪💪💪💪💪💪💪💪💪*/
 
 //Vowel Counter - How many vowels are there?
